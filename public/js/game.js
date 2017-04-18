6ccc05fbcdc9f38b7f9b0f5ea31bc7da1a81050f
Jeff Wu <makerjeffwu@gmail.com>
2017-04-18 10:56:05 -0700

    continue on second level entities

--- public/js/game.js
+++ public/js/game.js
@@ -2,6 +2,18 @@
  * Created by jeffersonwu on 4/11/17.
  */
 
+// Box2D Declarations
+var b2Vec2 = Box2D.Common.Math.b2Vec2;
+var b2BodyDef = Box2D.Dynamics.b2BodyDef;
+var b2Body = Box2D.Dynamics.b2Body;
+var b2FixtureDef = Box2D.Dynamics.b2FixtureDef;
+var b2Fixture = Box2D.Dynamics.b2Fixture;
+var b2World = Box2D.Dynamics.b2World;
+var b2PolygonShape = Box2D.Collision.Shapes.b2PolygonShape;
+var b2CircleShape = Box2D.Collision.Shapes.b2CircleShape;
+var b2DebugDraw = Box2D.Dynamics.b2DebugDraw;
+
+
 // TODO: requestAnimationFrame polyfill (invokes immediately)
 (function(){
     //TODO: continue here
@@ -182,9 +194,25 @@ var levels = {
         {   // first level
             foreground: 'desert-foreground',
             background: 'clouds-background',
-            entities: []
+            entities: [
+                {type: 'ground', name: 'dirt', x:500, y: 440, width: 1000, height: 20, isStatic: true},
+                {type: 'ground', name: 'wood', x:180, y: 390, width: 40, height: 80, isStatic: true},
+
+                {type: 'block', name: 'wood', x: 520, y: 375, angle: 90, width: 100, height: 25},
+                {type: 'block', name: 'glass', x: 520, y: 275, angle: 90, width: 100, height: 25},
+                {type: 'villain', name: 'burger', x: 520, y: 200, calories: 590},
+
+                {type: 'block', name: 'wood', x: 620, y: 375, angle: 90, width: 100, height: 25},
+                {type: 'block', name: 'glass', x:620, y: 275, angle: 90, width: 100, height: 25},
+                {type: 'villain', name: 'fries', x: 620, y: 200, calories: 420},
+
+                {type: 'hero', name: 'orange', x: 90, y: 410},
+                {type: 'hero', name: 'apple', x: 150, y: 410}
+            ]
         },
         {   // second level
+
+            //TODO: continue here.
             foreground: 'desert-foreground',
             background: 'clouds-background',
             entities: []
@@ -340,6 +368,197 @@ var mouse = {
     mouse_up_handler: function(ev) {
         mouse.down = false;
         mouse.dragging = false;
+    }
+};
+
+// ==============================
+// ENTITIES OBJECT ==============
+// ==============================
+var entities = {
+    definitions: {
+        "glass": {
+            full_health: 100,
+            density: 2.4,
+            friction: 0.4,
+            restitution: 0.15
+        },
+        "wood": {
+            full_health: 500,
+            density: 0.7,
+            friction: 0.4,
+            restitution: 0.4
+        },
+        "dirt": {
+            density: 3.0,
+            friction: 1.5,
+            restitution: 0.2
+        },
+        "burger": {
+            shape: 'circle',
+            full_health: 40,
+            radius: 25,
+            density: 1,
+            friction: 0.5,
+            restitution: 0.4
+        },
+        "sodacan": {
+            shape: 'rectangle',
+            full_health: 80,
+            width: 40,
+            height: 60,
+            density: 1,
+            friction: 0.5,
+            restitution: 0.7
+        },
+        "fries": {
+            shape: 'rectangle',
+            full_health: 50,
+            width: 40,
+            height: 50,
+            density: 1,
+            friction: 0.5,
+            restitution: 0.6
+        },
+        "apple": {
+            shape: 'circle',
+            radius: 25,
+            density: 1.5,
+            friction: 0.5,
+            restitution: 0.4
+        },
+        "strawberry": {
+            shape: 'circle',
+            radius: 15,
+            density: 2.0,
+            friction: 0.5,
+            restitution: 0.4
+        }
+    },
+
+    // take entity, create a Box2D body, add it to the world.
+    create: function(entity) {
+        var definition = entities.definitions[entity.name];
+
+        if(!definition) {
+            console.log ('Undefined entity name ' + entity.name);
+            return;
+        }
+
+        switch (entity.type) {
+            case 'block':
+                entity.health = definition.full_health;
+                entity.full_health = definition.full_health;
+                entity.shape = 'rectangle';
+                entity.sprite = loader.load_image('images/entities/' + entity.name + '.png');
+                box2d.create_rectangle(entity, definition);
+                break;
+            case 'ground':
+                // no need for health. these are indestructible
+                entity.shape = 'rectangle';
+                // no need for sprites. these won't be drawn. background art includes the ground.
+                box2d.create_rectangle(entity, definition);
+                break;
+            case 'hero':
+            case 'villain':
+                entity.health = definition.full_health;
+                entity.full_health = definition.full_health;
+                entity.sprite = loader.load_image('/images/entities/' + entity.name + '.png');
+                entity.shape = definition.shape;
+
+                if (definition.shape == 'circle') {
+                    entity.radius = definition.radius;
+                    box2d.create_circle(entity, definition);
+                }
+                else if (definition.shape == 'rectangle') {
+                    entity.width = definition.width;
+                    entity.height = definition.height;
+                    box2d.create_rectangle(entity, definition);
+                }
+                break;
+            default:
+                console.log('Undefined entity type', entity.type);
+                break;
+
+        }
+
+    },
+
+    // take entity, its position, and its angle and draw it on the game canvas.
+    draw: function(entity, position, angle) {
+
+    }
+};
+
+// ==============================
+// BOX2D OBJECT =================
+// ==============================
+var box2d = {
+    scale: 30,
+    init: function() {
+        // set up the Box2D world that will do most of the physics calculation
+        var gravity = new b2Vec2(0, 9.8);   // declare gravity
+        var allow_sleep = true;  // allow objects to go to sleep
+        box2d.world = new b2World(gravity, allow_sleep);
+    },
+
+    create_rectangle: function(entity, definition) {
+        var body_def = new b2BodyDef;
+        if (entity.isStatic) {
+            body_def.type = b2Body.b2_staticBody;
+        } else {
+            body_def.type = b2Body.b2_dynamicBody;
+        }
+
+        body_def.position.x = entity.x / box2d.scale;
+        body_def.position.y = entity.y / box2d.scale;
+
+        if (entity.angle) {
+            body_def.angle = Math.PI * entity.angle / 180;
+        }
+
+        var fixture_def = new b2FixtureDef;
+        fixture_def.density = definition.density;
+        fixture_def.friction = definition.friction;
+        fixture_def.restitution = definition.restitution;
+
+        fixture_def.shape = new b2PolygonShape;
+        fixture_def.shape.SetAsBox(entity.width / 2 / box2d.scale, entity.height / 2 / box2d.scale);
+
+        var body = box2d.world.CreateBody(body_def);
+        body.SetUserData(entity);
+
+        var fixture = body.CreateFixture(fixture_def);
+        return body;
+    },
+
+    create_circle: function(entity, definition) {
+        var body_def = new b2BodyDef;
+
+        if (entity.isStatic) {
+            body_def.type = b2Body.b2_staticBody;
+        } else {
+            body_def.type = b2Body.b2_dynamicBody;
+        }
+
+        body_def.position.x = entity.x / box2d.scale;
+        body_def.position.y = entity.y / box2d.scale;
+
+        if (entity.angle) {
+            body_def.angle = Math.PI*entity.angle / 180;
+        }
+
+        var fixture_def = new b2FixtureDef;
+        fixture_def.density = definition.density;
+        fixture_def.friction = definition.friction;
+        fixture_def.restitution = definition.restitution;
+
+        fixture_def.shape = new b2CircleShape(entity.radius / box2d.scale);
+
+        var body = box2d.world.CreateBody(body_def);
+        body.SetUserData(entity);
+
+        var fixture = body.CreateFixture(fixture_def);
+        return body;
     }
 };
 
